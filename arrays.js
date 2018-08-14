b566cdad22706e9922cc49081de102c80a423280
Alejandro Gonzalez <alejandrogxv@gmail.com>
2018-08-14 13:27:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,4 +19,6 @@ function destructivelyAddElementToEndOfArray(array,element){
   return array
 }
 
-function accessElementInArray(array, )
+function accessElementInArray(array, index){
+  return 
+}
